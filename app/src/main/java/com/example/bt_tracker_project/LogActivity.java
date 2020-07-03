f147c7611a856f72ae38fe8f8ca1a97e8687b6bc
2020m023HK <65376489+2020m023HK@users.noreply.github.com>
2020-07-03 12:55:29 +0900

    added onClick method to 2 buttons of activity_main.xml

--- app/src/main/java/com/example/bt_tracker_project/LogActivity.java
+++ app/src/main/java/com/example/bt_tracker_project/LogActivity.java
@@ -2,7 +2,9 @@ package com.example.bt_tracker_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class LogActivity extends AppCompatActivity {
 
@@ -11,4 +13,6 @@ public class LogActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log);
     }
+
+
 }
--- app/src/main/java/com/example/bt_tracker_project/MainActivity.java
+++ app/src/main/java/com/example/bt_tracker_project/MainActivity.java
@@ -25,13 +25,7 @@ public class MainActivity extends AppCompatActivity {
         startActivity(i1);
     }
 
-    public void openMechanismLink(View view) {
-        Intent i2 = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.scientificamerican.com/article/what-causes-a-fever/"));
-        startActivity(i2);
-    }
 
-    public void openHandleFeverLink(View view) {
-        Intent i3 = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.healthline.com/health/how-to-break-a-fever"));
-        startActivity(i3);
-    }
+
+
 }
