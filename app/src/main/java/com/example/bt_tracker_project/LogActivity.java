9fa9793dde0890b9d46b31b2750cdeb1dace1505
2020m023HK <65376489+2020m023HK@users.noreply.github.com>
2020-07-17 04:53:03 +0900

    created empty activity added button and textview added android:onClick="confirmBT" added public void confirmBT

--- app/src/main/java/com/example/bt_tracker_project/LogActivity.java
+++ app/src/main/java/com/example/bt_tracker_project/LogActivity.java
@@ -12,7 +12,17 @@ public class LogActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log);
+        //get the ID of the EditText on the layout
+        userInput = (EditText) findViewById(R.id.LogInputData);
+}
+    public void confirmBT(View view) {
+        Intent toConfirm = new Intent(this, ConfirmActivity.class);
+        //append user input to the toConfirm Intent
+        //the 2nd argument means: get user input from the EditText View,
+        //and convert the text to a string
+        toConfirm.putExtra("BT_data",userInput.getText().toString());
+        //start ConfirmActivity
+        startActivity(toConfirm);
     }
-
 
 }
