ae2a14fdcdc7c526dfcba213bded9185b155390d
2020m023HK <65376489+2020m023HK@users.noreply.github.com>
2020-07-17 03:40:45 +0900

    added button of "BACK TO HOME", added android:onClick="normalToHome" added android.content.Intent; added public void

--- app/src/main/java/com/example/bt_tracker_project/NormalActivity.java
+++ app/src/main/java/com/example/bt_tracker_project/NormalActivity.java
@@ -4,16 +4,25 @@ import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
 
-public class NormalActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_normal);
     }
-
+        TextView mainButton1 = findViewById(R.id.MainButton1);
+        MainButton1.setOnClickListener(this);
+    @Override
     public void normalToHome(View view) {
-        Intent i = new Intent(this,MainActivity.class);
-        startActivity(i);
+
+        switch (view.getId()){
+            case R.id.NainButton1:
+                //If Mainbutton1 is clicked, do something
+                Intent i = new Intent(this,MainActivity.class);
+                startActivity(i);
+                break;
+        }
     }
 }
